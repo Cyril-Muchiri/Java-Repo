04fa595d6646a053283196ad75e270433bb34163
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-09-01 13:12:42 +0300

    question 2 done

---
+++ Java-SE-8-Programmer-I/practice/src/main/java/com/chief/Question2.java
@@ -0,0 +1,12 @@
+package com.chief;
+
+public class Question2 {
+    public static void main(String[] args) {
+        int x =9;
+        if(x++<10){
+System.out.println(x + " Hello world!");
+        }else{
+            System.out.println(x + " Hello universe!!");
+        }
+    }
+}
