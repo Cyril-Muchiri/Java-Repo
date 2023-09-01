f950ed94b5bd57a8f81c6e81db2a99c620a9914e
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2023-09-01 13:05:13 +0300

    question 1 done

---
+++ Java-SE-8-Programmer-I/practice/src/main/java/com/chief/App.java
@@ -0,0 +1,13 @@
+package com.chief;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Java-SE-8-Programmer-I/practice/src/main/java/com/chief/Question1.java
@@ -0,0 +1,25 @@
+package com.chief;
+
+class Product {
+    double price;
+}
+
+public class Question1 {
+    public void updatePrice(Product product, double price) {
+        price = price * 2;
+        product.price = product.price + price;
+
+    }
+
+    public static void main(String[] args) {
+        Product prt = new Product();
+        prt.price = 200;
+        double newPrice = 100;
+
+        Question1 q = new Question1();
+        q.updatePrice(prt, newPrice);
+
+        System.out.println(prt.price + " : " + newPrice);
+    }
+
+}
